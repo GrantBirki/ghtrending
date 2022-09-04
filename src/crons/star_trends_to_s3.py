95c7b3a5fab0f11e34ef3a900b1128f497e42bbc
Grant Birkinbine <grantbirki@github.com>
2022-09-04 10:55:00 -0600

    add some print statements

--- src/crons/star_trends_to_s3.py
+++ src/crons/star_trends_to_s3.py
@@ -40,21 +40,29 @@ def main():
     results = []
 
     # Get the most stared repos from all time from the DB
+    print("Getting most stared repos from all time")
     results.append({"name": "all_time", "data": star_events.get_most_stared()})
 
     # Get the most stared repos from the past 24 hours
+    print("Getting most stared repos from the past 24 hours")
     results.append({"name": "last_24_hours", "data": star_events.get_most_stared(hours=24)})
 
     # Get the most stared repos from the past 7 days
+    print("Getting most stared repos from the past 7 days")
     results.append({"name": "last_7_days", "data": star_events.get_most_stared(hours=24 * 7)})
 
     # Get the most stared repos from the past 30 days
+    print("Getting most stared repos from the past 30 days")
     results.append({"name": "last_30_days", "data": star_events.get_most_stared(hours=24 * 30)})
 
     star_events.close()
 
+    # Upload to S3
+    print("Uploading to S3...")
     for result in results:
         upload_to_s3(result)
+
+    print("Done!")
 
 if __name__ == "__main__":
     main()
