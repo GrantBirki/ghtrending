17a9fd7196b968a87f8adb3c74f781b54f96f38f
Grant Birkinbine <grantbirki@github.com>
2022-09-04 14:27:49 -0600

    remove next.js

--- src/pages/_app.jsx
+++
@@ -1,5 +0,0 @@
-import '../styles/global.css';
-
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
-
-export default MyApp;
--- src/utils/AppConfig.js
+++
@@ -1,8 +0,0 @@
-const AppConfig = {
-  site_name: 'ghtrending',
-  title: 'ghtrending',
-  description: 'Trending GitHub Repositories',
-  locale: 'en',
-};
-
-export default AppConfig;
