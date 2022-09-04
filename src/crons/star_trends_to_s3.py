c09359cca500f20ee209b992ae4d337590c299b1
Grant Birkinbine <grantbirki@github.com>
2022-09-04 11:07:50 -0600

    set cache control and fmt

--- src/crons/star_trends_to_s3.py
+++ src/crons/star_trends_to_s3.py
@@ -1,7 +1,7 @@
+import json
 import os
 import sys
 from pathlib import Path
-import json
 
 import boto3
 
@@ -13,13 +13,20 @@ from stars import StarEvents
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", None)
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", None)
 BUCKET_NAME = os.environ.get("BUCKET_NAME", "data.ghtrending.io")
+AWS_REGION = os.environ.get("AWS_REGION", "us-east-1")
 STAR_TRENDS_PATH = os.environ.get("STAR_TRENDS_PATH", "trends/stars")
+CACHE_CONTROL = str(os.environ.get("CACHE_CONTROL", "7200"))
+
 
 def upload_to_s3(result):
+    """
+    Uploads files to S3
+    """
     s3 = boto3.client(
         "s3",
         aws_access_key_id=AWS_ACCESS_KEY_ID,
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_REGION,
     )
 
     s3.put_object(
@@ -28,6 +35,26 @@ def upload_to_s3(result):
         Body=json.dumps(result["data"]),
         ContentType="application/json",
     )
+
+
+def set_cache_control():
+    """
+    Sets cache control headers for all objects in the STAR_TRENDS_PATH/* directory
+    """
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(BUCKET_NAME)
+    for summary in bucket.objects.filter(Prefix=f"{STAR_TRENDS_PATH}/"):
+        obj = summary.Object()
+        obj.copy_from(
+            CopySource={
+                "Bucket": BUCKET_NAME,
+                "Key": obj.key,
+            },
+            Metadata=obj.metadata,  # This copies existing metadata
+            CacheControl=f"public,max-age={CACHE_CONTROL}",
+            MetadataDirective="REPLACE",
+        )
+
 
 def main():
     if AWS_ACCESS_KEY_ID is None or AWS_SECRET_ACCESS_KEY is None:
@@ -45,15 +72,21 @@ def main():
 
     # Get the most stared repos from the past 24 hours
     print("Getting most stared repos from the past 24 hours")
-    results.append({"name": "last_24_hours", "data": star_events.get_most_stared(hours=24)})
+    results.append(
+        {"name": "last_24_hours", "data": star_events.get_most_stared(hours=24)}
+    )
 
     # Get the most stared repos from the past 7 days
     print("Getting most stared repos from the past 7 days")
-    results.append({"name": "last_7_days", "data": star_events.get_most_stared(hours=24 * 7)})
+    results.append(
+        {"name": "last_7_days", "data": star_events.get_most_stared(hours=24 * 7)}
+    )
 
     # Get the most stared repos from the past 30 days
     print("Getting most stared repos from the past 30 days")
-    results.append({"name": "last_30_days", "data": star_events.get_most_stared(hours=24 * 30)})
+    results.append(
+        {"name": "last_30_days", "data": star_events.get_most_stared(hours=24 * 30)}
+    )
 
     star_events.close()
 
@@ -62,7 +95,12 @@ def main():
     for result in results:
         upload_to_s3(result)
 
+    print(f"Setting Cache-Control for {STAR_TRENDS_PATH}/* objects...")
+
+    set_cache_control()
+
     print("Done!")
+
 
 if __name__ == "__main__":
     main()
