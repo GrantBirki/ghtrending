b8e7281dc797f5b503fa32cd51be49e9f0a003bc
Grant Birkinbine <grantbirki@github.com>
2022-09-05 16:40:38 -0600

    try to fix keys

--- src/components/stars/index.js
+++ src/components/stars/index.js
@@ -27,7 +27,8 @@ function Stars() {
 
   return (
     <>
-      {stars.map((star) => {
+      {stars.map((star, index) => {
+        var keyIndex = `${star.repo_name}-${index}`;
         let repoOwner = star.repo_name.split("/")[0];
         let repoName = star.repo_name.split("/")[1];
         let langFmt = star.language;
@@ -37,28 +38,28 @@ function Stars() {
 
         return (
           <>
-            <Box className="table-row" key={`${star.repo_url}-repo-row-div`}>
+            <Box className="table-row" key={`${keyIndex}-repo-row-div`}>
               <Text
-                key={`${star.repo_url}-repo-icon-text`}
+                key={`${keyIndex}-repo-icon-text`}
                 sx={{
                   marginRight: "7px",
                 }}
               >
-                <RepoIcon key={`${star.repo_url}-repo-icon`} size={16} />
+                <RepoIcon key={`${keyIndex}-repo-icon`} size={16} />
               </Text>
               <Link
-                key={`${star.repo_url}-repo-link`}
+                key={`${keyIndex}-repo-link`}
                 sx={{ fontSize: 3 }}
                 href={star.repo_url}
               >
-                <Text key={`${star.repo_url}-repo-owner`}>{repoOwner}</Text>
-                <Text key={`${star.repo_url}-repo-slash`}> / </Text>
-                <Text key={`${star.repo_url}-repo-name`} fontWeight="bold">
+                <Text key={`${keyIndex}-repo-owner`}>{repoOwner}</Text>
+                <Text key={`${keyIndex}-repo-slash`}> / </Text>
+                <Text key={`${keyIndex}-repo-name`} fontWeight="bold">
                   {repoName}
                 </Text>
               </Link>
               <Text
-                key={`${star.repo_url}-repo-desc`}
+                key={`${keyIndex}-repo-desc`}
                 sx={{
                   marginTop: "3px",
                   marginBottom: "3px",
@@ -67,29 +68,29 @@ function Stars() {
               >
                 {star.description}
               </Text>
-              <Box key={`${star.repo_url}-repo-info-div`}>
+              <Box key={`${keyIndex}-repo-info-div`}>
                 <LanguageColor
-                  key={`${star.repo_url}-repo-lang-comp`}
+                  key={`${keyIndex}-repo-lang-comp`}
                   lang={star.language}
                   repo_url={star.repo_url}
                 />
                 <Text
-                  key={`${star.repo_url}-repo-langfmt`}
+                  key={`${keyIndex}-repo-langfmt`}
                   fontSize={"12px"}
                   color="neutral.emphasisPlus"
                 >
                   {langFmt}
                 </Text>
                 <Text
-                  key={`${star.repo_url}-repo-stars-count-wrapper`}
+                  key={`${keyIndex}-repo-stars-count-wrapper`}
                   sx={{ float: "right" }}
                 >
                   <Text
-                    key={`${star.repo_url}-repo-stars-count-wrapper-margin`}
+                    key={`${keyIndex}-repo-stars-count-wrapper-margin`}
                     sx={{ marginRight: "4px" }}
                   >
                     <StarIcon
-                      key={`${star.repo_url}-repo-stars-count-icon`}
+                      key={`${keyIndex}-repo-stars-count-icon`}
                       size={16}
                     />
                   </Text>
