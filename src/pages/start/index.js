0cc171109e06aebf94ddb4342fa556e176468bc8
Grant Birkinbine <grantbirki@github.com>
2022-09-05 13:56:11 -0600

    css improvements with react primer components

--- src/pages/start/index.js
+++ src/pages/start/index.js
@@ -7,22 +7,22 @@ import "./index.css";
 function Start() {
   return (
     <>
-      <div className="center">
+      <Box bg="canvas.default" className="center">
         <div className="border-bottom main-header bg-color-muted">
           <h1>Trending</h1>
           <p>
             See what the GitHub community is most excited about today.
           </p>
         </div>
-      </div>
-      <Box>
+      </Box>
+      <Box bg="canvas.default" className="primary-header">
         <div className="container-lg">
-          <div className="container-lg-header bg-color-muted">
+          <Box bg="canvas.subtle" className="container-lg-header">
             <div></div>
             <div className="table-header-options">
               <DateRangeToggle />
             </div>
-          </div>
+          </Box>
           <div className="container-table">
             <Stars />
           </div>
