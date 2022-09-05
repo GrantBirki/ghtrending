c33b7e526cfd8d0b040c62aab4b31722b20dc401
Grant Birkinbine <grantbirki@github.com>
2022-09-04 23:15:29 -0600

    adding more data to table

--- src/components/stars/index.js
+++ src/components/stars/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { Link, Text } from "@primer/react";
 
 import fetchStars from "../../services/fetchStars";
+import "./index.css";
 
 function Stars() {
   const [stars, setStars] = useState("");
@@ -25,8 +27,37 @@ function Stars() {
   return (
     <>
       {stars.map((star) => {
-        var repoName = star.repo_name;
-        return <p>{repoName}</p>;
+        let repoOwner = star.repo_name.split("/")[0];
+        let repoName = star.repo_name.split("/")[1];
+
+        return (
+          <>
+            <div className="table-row">
+              <Link sx={{ fontSize: 3 }} href={star.repo_url}>
+                <Text as="span">{repoOwner}</Text>
+                <Text as="span"> / </Text>
+                <Text as="span" fontWeight="bold">
+                  {repoName}
+                </Text>
+              </Link>
+              <Text
+                sx={{
+                  marginTop: "3px",
+                  marginBottom: "3px",
+                }}
+                as={"p"}
+                color="neutral"
+              >
+                {star.description}
+              </Text>
+              <div className="f6 color-fg-muted mt-2">
+                <Text fontSize={"12px"} color="neutral.emphasisPlus" as="span">
+                  {star.language}
+                </Text>
+              </div>
+            </div>
+          </>
+        );
       })}
     </>
   );
