73b6d85dcce154cc0c46557327411f8f6ad799f2
Grant Birkinbine <grant.birkinbine@gmail.com>
2023-02-28 22:19:03 +0000

    testing branch deployments

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -11,7 +11,7 @@ function About() {
             About
           </Heading>
           <Box className="center">
-            <Text padding={"3rem"} as={"p"}>ghtrending is an open source site to aggregate and display various "trending" information about GitHub. This project is open source and made for the community to help people discover new and exciting GitHub projects!
+            <Text padding={"3rem"} as={"p"}>ghtrending is an open source site to aggregate and display various "trending" information about GitHub. This project is open source and made for the community to help people discover new and exciting GitHub projects!!
             </Text>
             <Text>
               Source Code:{" "}
