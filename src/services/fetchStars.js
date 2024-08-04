11a07e1791279b0ca1701a511461a0d19034aca0
Grant Birkinbine <grant.birkinbine@gmail.com>
2024-08-04 00:34:27 -0700

    use subdomain for `data` because CORS

--- src/services/fetchStars.js
+++ src/services/fetchStars.js
@@ -1,6 +1,6 @@
 const DEFAULT_PATH = "trends/stars";
 const SUFFIX = ".json";
-const BASE_URL = "https://ghtrendingdata.birki.io";
+const BASE_URL = "https://data.ghtrending.birki.io";
 
 async function fetchStars(path) {
   const response = await fetch(`${BASE_URL}/${DEFAULT_PATH}/${path}${SUFFIX}`, {
